061c847ec1507ec286600e55a234a445f3c42900
VolkanGurcan <volkan.gurcan@emo.org.tr>
2026-03-10 22:07:09 +1100

    Fix comment formatting in CodeDemo.cpp

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -1,9 +1,8 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
-
+// Hello World, by Eduardo Corpeño
 #include <iostream>
-
+// Test
 int main(){
     std::cout << "Hi There!" << std::endl;
 
